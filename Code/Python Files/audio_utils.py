06bdbe8406b3cc2e1f2400f3ec2346aaeccc2bc8
Meyuhasson <76136081+Meyuhasson@users.noreply.github.com>
2021-09-21 18:29:36 +0300

    update

--- Code/Python Files/audio_utils.py
+++ Code/Python Files/audio_utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import librosa.display
 from IPython.display import display
 import seaborn as sns
 from scipy import stats
@@ -9,13 +10,149 @@ import matplotlib.pyplot as plt
 import IPython.display as ipd
 import librosa.display
 import os
+from tqdm import tqdm
 
-def get_list_of_wav_files(BENIGN_RECORDING_PATH):
+
+# the time slice in second, which we analyse in the function.
+TIME_SLICE = 1
+
+def write_wav_features_to_csv(list_of_benign_wavFiles_paths, list_of_malicious_wavFiles_paths, output_path):
+
+    FIRST_RUN_INDICATOR = True
+    i = 0
+    for wavFilePath in tqdm(list_of_benign_wavFiles_paths):
+
+        # path for load the audio file(wav)
+        WAVEFILE_PATH = wavFilePath
+
+        #reading data and sample rate, sample rate is by deafult value 22050, and it's defines the audio speed
+        data, sr = librosa.load(WAVEFILE_PATH)
+
+        #audio_utils.amplitude_plot_byTime(TIME_SLICE, data, sr)
+        #audio_utils.stft_plot(data, sr)
+
+        duration = getTime(data)
+        original_sr = sampleRate_value(WAVEFILE_PATH)
+        extermom_point_diff_dict = Extermom_Points_Change(TIME_SLICE, data)
+        suspicious_diff = Suspicious_Change_In_Extermom(extermom_point_diff_dict)
+        min = minimum_amplitude(data)
+        max = maximum_amplitude(data)
+        max_magnitude,min_magnitude, max_phase, min_phase = stft(data, sr)
+        dict_of_RTPSPLIT_features = get_rtp_features_of_RTPSPLIT(wavFilePath.replace("_.wav", ".txt"))
+        RTP_ssrc = dict_of_RTPSPLIT_features["RTP ssrc"]
+        RTP_payload_type = dict_of_RTPSPLIT_features["RTP payload type"]
+        Flushed_packets = dict_of_RTPSPLIT_features["Flushed packets"]
+        Lost_packets = dict_of_RTPSPLIT_features["Lost packets"]
+        RTP_payload_length = dict_of_RTPSPLIT_features["RTP payload length"]
+        print ("the duration of the audio file is: "+ str(duration))
+        print ("the original sample rate of the audio file is: "+ str(original_sr))
+        print ("is the audio file extermom points are chaged Suspiciously :" + str(suspicious_diff))
+        print ("min point of the audio file: " + str(min))
+        print ("max point of the audio file: " + str(max))
+        print ("5 max values in magnitude complex matrix are: " + str(max_magnitude))
+        print ("5 min values in magnitude complex matrix are: " + str(min_magnitude))
+        print("5 max values in phase complex matrix are: " + str(max_phase))
+        print("5 min values in phase complex matrix are: " + str(min_phase))
+        df = pd.DataFrame()
+        df._set_value(i, 'duration', duration)
+        df._set_value(i, 'original_sr', original_sr)
+        df._set_value(i, 'suspicious_diff', suspicious_diff)
+        df._set_value(i, 'min amplitude', min)
+        df._set_value(i, 'max amplitude', max)
+        df._set_value(i, 'max_magnitude1', max_magnitude[0])
+        df._set_value(i, 'max_magnitude2', max_magnitude[1])
+        df._set_value(i, 'max_magnitude3', max_magnitude[2])
+        df._set_value(i, 'min_magnitude1', min_magnitude[0])
+        df._set_value(i, 'min_magnitude2', min_magnitude[1])
+        df._set_value(i, 'min_magnitude3', min_magnitude[2])
+        df._set_value(i, 'max_phase1', max_phase[0])
+        df._set_value(i, 'max_phase2', max_phase[1])
+        df._set_value(i, 'max_phase3', max_phase[2])
+        df._set_value(i, 'min_phase1', min_phase[0])
+        df._set_value(i, 'min_phase2', min_phase[1])
+        df._set_value(i, 'min_phase3', min_phase[2])
+        df._set_value(i, 'RTP_ssrc', RTP_ssrc)
+        df._set_value(i, 'RTP_payload_length', RTP_payload_length)
+        df._set_value(i, 'RTP_payload_type', RTP_payload_type)
+        df._set_value(i, 'Lost_packets', Lost_packets)
+        df._set_value(i, 'Flushed_packets', Flushed_packets)
+        df._set_value(i, 'isMalicious', False)
+
+        if (FIRST_RUN_INDICATOR == True):
+            df.to_csv(output_path, encoding='utf-8')
+            FIRST_RUN_INDICATOR = False
+            i+= 1
+        else:
+            df.to_csv(output_path, mode='a', header=False)
+            i+= 1
+
+    for wavFilePath in tqdm(list_of_malicious_wavFiles_paths):
+        # path for load the audio file(wav)
+        WAVEFILE_PATH = wavFilePath
+
+        # reading data and sample rate, sample rate is by deafult value 22050, and it's defines the audio speed
+        data, sr = librosa.load(WAVEFILE_PATH)
+
+        # audio_utils.amplitude_plot_byTime(TIME_SLICE, data, sr)
+        # audio_utils.stft_plot(data, sr)
+
+        duration = getTime(data)
+        original_sr = sampleRate_value(WAVEFILE_PATH)
+        extermom_point_diff_dict = Extermom_Points_Change(TIME_SLICE, data)
+        suspicious_diff = Suspicious_Change_In_Extermom(extermom_point_diff_dict)
+        min = minimum_amplitude(data)
+        max = maximum_amplitude(data)
+        max_magnitude, min_magnitude, max_phase, min_phase = stft(data, sr)
+        dict_of_RTPSPLIT_features = get_rtp_features_of_RTPSPLIT(wavFilePath.replace("_.wav",".txt"))
+        RTP_ssrc = dict_of_RTPSPLIT_features["RTP ssrc"]
+        RTP_payload_type = dict_of_RTPSPLIT_features["RTP payload type"]
+        Flushed_packets = dict_of_RTPSPLIT_features["Flushed packets"]
+        Lost_packets = dict_of_RTPSPLIT_features["Lost packets"]
+        RTP_payload_length = dict_of_RTPSPLIT_features["RTP payload length"]
+        print("the duration of the audio file is: " + str(duration))
+        print("the original sample rate of the audio file is: " + str(original_sr))
+        print("is the audio file extermom points are chaged Suspiciously :" + str(suspicious_diff))
+        print("min point of the audio file: " + str(min))
+        print("max point of the audio file: " + str(max))
+        print("5 max values in magnitude complex matrix are: " + str(max_magnitude))
+        print("5 min values in magnitude complex matrix are: " + str(min_magnitude))
+        print("5 max values in phase complex matrix are: " + str(max_phase))
+        print("5 min values in phase complex matrix are: " + str(min_phase))
+        df = pd.DataFrame()
+        df._set_value(i, 'duration', duration)
+        df._set_value(i, 'original_sr', original_sr)
+        df._set_value(i, 'suspicious_diff', suspicious_diff)
+        df._set_value(i, 'min amplitude', min)
+        df._set_value(i, 'max amplitude', max)
+        df._set_value(i, 'max_magnitude1', max_magnitude[0])
+        df._set_value(i, 'max_magnitude2', max_magnitude[1])
+        df._set_value(i, 'max_magnitude3', max_magnitude[2])
+        df._set_value(i, 'min_magnitude1', min_magnitude[0])
+        df._set_value(i, 'min_magnitude2', min_magnitude[1])
+        df._set_value(i, 'min_magnitude3', min_magnitude[2])
+        df._set_value(i, 'max_phase1', max_phase[0])
+        df._set_value(i, 'max_phase2', max_phase[1])
+        df._set_value(i, 'max_phase3', max_phase[2])
+        df._set_value(i, 'min_phase1', min_phase[0])
+        df._set_value(i, 'min_phase2', min_phase[1])
+        df._set_value(i, 'min_phase3', min_phase[2])
+        df._set_value(i, 'RTP_ssrc', RTP_ssrc)
+        df._set_value(i, 'RTP_payload_length', RTP_payload_length)
+        df._set_value(i, 'RTP_payload_type', RTP_payload_type)
+        df._set_value(i, 'Lost_packets', Lost_packets)
+        df._set_value(i, 'Flushed_packets', Flushed_packets)
+        df._set_value(i, 'isMalicious', True)
+
+        df.to_csv(output_path, mode='a', header=False)
+        i+=1
+
+#get a directory path and the desired file type as string and return a list of files from that type.
+def get_list_of_files_fromType(RECORDING_PATH, type):
     list_of_wav_files = []
-    for dir in os.listdir(BENIGN_RECORDING_PATH):
-        for file in os.listdir(BENIGN_RECORDING_PATH + '\\' + dir):
-            if file.endswith(".wav"):
-                list_of_wav_files.append(os.path.join(BENIGN_RECORDING_PATH + "\\" + dir, file))
+    for dir in os.listdir(RECORDING_PATH):
+        for file in os.listdir(RECORDING_PATH + '\\' + dir):
+            if file.endswith("."+type):
+                list_of_wav_files.append(os.path.join(RECORDING_PATH + "\\" + dir, file))
     return list_of_wav_files
 
 def getTime(inner_data):
@@ -114,3 +251,13 @@ def stft(inner_data, sr):
         phase_of_freq_min[np.unravel_index(phase_of_freq_min.argmin(), phase_of_freq_min.shape)] = phase_of_freq_max[np.unravel_index(phase_of_freq_max.argmax(), phase_of_freq_max.shape)]
 
     return max_magnitude,min_magnitude, max_phase, min_phase
+
+#get txt file path with rtpsplit output and return the features out of it
+def get_rtp_features_of_RTPSPLIT(file_path):
+    dict_of_rtp_features = {}
+    with open(file_path.replace("wav", "txt")) as fh:
+        for line in fh:
+            if line.strip():  # Ignore blank lines
+                key, value = [x.strip() for x in line.strip().split(':', 1)]
+                dict_of_rtp_features[key] = value
+    return dict_of_rtp_features
--- Code/Python Files/main.py
+++ Code/Python Files/main.py
@@ -1,15 +1,5 @@
-import pandas as pd
 import numpy as np
-import os
-from IPython.display import display
-import seaborn as sns
-from scipy import stats
-from sklearn.feature_selection import SelectKBest, mutual_info_classif
-import sklearn
-import matplotlib.pyplot as plt
-import IPython.display as ipd
-from tqdm import tqdm
-import librosa.display
+
 import audio_utils
 
 np.set_printoptions(suppress=True) #prevent numpy exponential
@@ -23,117 +13,11 @@ BENIGN_RECORDING_PATH = r"C:\Users\edenm\Desktop\thesis\proccesed and tagged ext
 #the path consist files with wav files isnide them - only malicious
 MALICIOUS_RECORDING_PATH = r"C:\Users\edenm\Desktop\thesis\proccesed and tagged external datasets\RTP Flood records\malicious recordings"
 
-#the time slice in second, which we analyse in the function.
-TIME_SLICE = 1
 
-FIRST_RUN_INDICATOR = True
 
 if __name__ == '__main__':
 
-    list_of_benign_wav_files = audio_utils.get_list_of_wav_files(BENIGN_RECORDING_PATH)
-    list_of_malicious_wav_files = audio_utils.get_list_of_wav_files(MALICIOUS_RECORDING_PATH)
-    i = 0
+    list_of_benign_wav_files = audio_utils.get_list_of_files_fromType(BENIGN_RECORDING_PATH, "wav")
+    list_of_malicious_wav_files = audio_utils.get_list_of_files_fromType(MALICIOUS_RECORDING_PATH, "wav")
+    audio_utils.write_wav_features_to_csv(list_of_benign_wav_files, list_of_malicious_wav_files, OUTPUT_PATH)
 
-    for wavFilePath in tqdm(list_of_benign_wav_files):
-
-        # path for load the audio file(wav)
-        WAVEFILE_PATH = wavFilePath
-
-        #reading data and sample rate, sample rate is by deafult value 22050, and it's defines the audio speed
-        data, sr = librosa.load(WAVEFILE_PATH)
-
-        #audio_utils.amplitude_plot_byTime(TIME_SLICE, data, sr)
-        #audio_utils.stft_plot(data, sr)
-
-        duration = audio_utils.getTime(data)
-        original_sr = audio_utils.sampleRate_value(WAVEFILE_PATH)
-        extermom_point_diff_dict = audio_utils.Extermom_Points_Change(TIME_SLICE, data)
-        suspicious_diff = audio_utils.Suspicious_Change_In_Extermom(extermom_point_diff_dict)
-        min = audio_utils.minimum_amplitude(data)
-        max = audio_utils.maximum_amplitude(data)
-        max_magnitude,min_magnitude, max_phase, min_phase = audio_utils.stft(data, sr)
-        print ("the duration of the audio file is: "+ str(duration))
-        print ("the original sample rate of the audio file is: "+ str(original_sr))
-        print ("is the audio file extermom points are chaged Suspiciously :" + str(suspicious_diff))
-        print ("min point of the audio file: " + str(min))
-        print ("max point of the audio file: " + str(max))
-        print ("5 max values in magnitude complex matrix are: " + str(max_magnitude))
-        print ("5 min values in magnitude complex matrix are: " + str(min_magnitude))
-        print("5 max values in phase complex matrix are: " + str(max_phase))
-        print("5 min values in phase complex matrix are: " + str(min_phase))
-        df = pd.DataFrame()
-        df._set_value(i, 'duration', duration)
-        df._set_value(i, 'original_sr', original_sr)
-        df._set_value(i, 'suspicious_diff', suspicious_diff)
-        df._set_value(i, 'min amplitude', min)
-        df._set_value(i, 'max amplitude', max)
-        df._set_value(i, 'max_magnitude1', max_magnitude[0])
-        df._set_value(i, 'max_magnitude2', max_magnitude[1])
-        df._set_value(i, 'max_magnitude3', max_magnitude[2])
-        df._set_value(i, 'min_magnitude1', min_magnitude[0])
-        df._set_value(i, 'min_magnitude2', min_magnitude[1])
-        df._set_value(i, 'min_magnitude3', min_magnitude[2])
-        df._set_value(i, 'max_phase1', max_phase[0])
-        df._set_value(i, 'max_phase2', max_phase[1])
-        df._set_value(i, 'max_phase3', max_phase[2])
-        df._set_value(i, 'min_phase1', min_phase[0])
-        df._set_value(i, 'min_phase2', min_phase[1])
-        df._set_value(i, 'min_phase3', min_phase[2])
-        df._set_value(i, 'isMalicious', False)
-
-        if (FIRST_RUN_INDICATOR == True):
-            df.to_csv(OUTPUT_PATH, encoding='utf-8')
-            FIRST_RUN_INDICATOR = False
-            i+= 1
-        else:
-            df.to_csv(OUTPUT_PATH, mode='a', header=False)
-            i+= 1
-
-    for wavFilePath in tqdm(list_of_malicious_wav_files):
-        # path for load the audio file(wav)
-        WAVEFILE_PATH = wavFilePath
-
-        # reading data and sample rate, sample rate is by deafult value 22050, and it's defines the audio speed
-        data, sr = librosa.load(WAVEFILE_PATH)
-
-        # audio_utils.amplitude_plot_byTime(TIME_SLICE, data, sr)
-        # audio_utils.stft_plot(data, sr)
-
-        duration = audio_utils.getTime(data)
-        original_sr = audio_utils.sampleRate_value(WAVEFILE_PATH)
-        extermom_point_diff_dict = audio_utils.Extermom_Points_Change(TIME_SLICE, data)
-        suspicious_diff = audio_utils.Suspicious_Change_In_Extermom(extermom_point_diff_dict)
-        min = audio_utils.minimum_amplitude(data)
-        max = audio_utils.maximum_amplitude(data)
-        max_magnitude, min_magnitude, max_phase, min_phase = audio_utils.stft(data, sr)
-        print("the duration of the audio file is: " + str(duration))
-        print("the original sample rate of the audio file is: " + str(original_sr))
-        print("is the audio file extermom points are chaged Suspiciously :" + str(suspicious_diff))
-        print("min point of the audio file: " + str(min))
-        print("max point of the audio file: " + str(max))
-        print("5 max values in magnitude complex matrix are: " + str(max_magnitude))
-        print("5 min values in magnitude complex matrix are: " + str(min_magnitude))
-        print("5 max values in phase complex matrix are: " + str(max_phase))
-        print("5 min values in phase complex matrix are: " + str(min_phase))
-        df = pd.DataFrame()
-        df._set_value(i, 'duration', duration)
-        df._set_value(i, 'original_sr', original_sr)
-        df._set_value(i, 'suspicious_diff', suspicious_diff)
-        df._set_value(i, 'min amplitude', min)
-        df._set_value(i, 'max amplitude', max)
-        df._set_value(i, 'max_magnitude1', max_magnitude[0])
-        df._set_value(i, 'max_magnitude2', max_magnitude[1])
-        df._set_value(i, 'max_magnitude3', max_magnitude[2])
-        df._set_value(i, 'min_magnitude1', min_magnitude[0])
-        df._set_value(i, 'min_magnitude2', min_magnitude[1])
-        df._set_value(i, 'min_magnitude3', min_magnitude[2])
-        df._set_value(i, 'max_phase1', max_phase[0])
-        df._set_value(i, 'max_phase2', max_phase[1])
-        df._set_value(i, 'max_phase3', max_phase[2])
-        df._set_value(i, 'min_phase1', min_phase[0])
-        df._set_value(i, 'min_phase2', min_phase[1])
-        df._set_value(i, 'min_phase3', min_phase[2])
-        df._set_value(i, 'isMalicious', True)
-
-        df.to_csv(OUTPUT_PATH, mode='a', header=False)
-        i+=1
