c08656a7aa52db5890b1349c667b763ea9a7bfcf
Meyuhasson <76136081+Meyuhasson@users.noreply.github.com>
2021-10-06 21:45:04 +0300

    decision tree classifier understanding and features correlation

--- Code/Python Files/DesicionTree_classifier.py
+++ Code/Python Files/DesicionTree_classifier.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn import tree
 from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
 
 
 PROCESSED_FEATURES_PATH = r"C:\Users\edenm\Documents\GitHub\VoIP-Research\Data\features_extracted\processed_features_for_train.csv"
@@ -9,9 +10,18 @@ data = pd.read_csv(PROCESSED_FEATURES_PATH).drop([r"Unnamed: 0", r"RTP_payload_t
 X = data.drop("isMalicious", axis=1)
 y = data.drop(data.columns.difference(["isMalicious"]), axis=1)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-model = tree.DecisionTreeClassifier()
-model.fit(X, y)
+clf = tree.DecisionTreeClassifier(criterion = "entropy")
+model = clf.fit(X, y)
 print(model.score(X_test, model.predict(X_test)))
+text_representation = tree.export_text(model)
+print(text_representation)
+fig = plt.figure(figsize=(10,5))
+_ = tree.plot_tree(clf,
+                   feature_names=list(X_train.columns.values),
+                   class_names= ["True", "False"],
+                   filled=True)
+plt.show()
+
 
 """
 to be continue:
