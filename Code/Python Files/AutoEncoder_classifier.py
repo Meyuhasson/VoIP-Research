2266324c59622fcc4db4c796171dfc8f89056ef8
Meyuhasson <76136081+Meyuhasson@users.noreply.github.com>
2021-10-20 23:33:37 +0300

    Update AutoEncoder_classifier.py

--- Code/Python Files/AutoEncoder_classifier.py
+++ Code/Python Files/AutoEncoder_classifier.py
@@ -1,60 +1,47 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from pyod.models.auto_encoder import AutoEncoder
+import numpy as np
+from matplotlib import pyplot as plt
+from numpy import sqrt, array, random, argsort
+from tensorflow import keras
+
 
 
 PROCESSED_FEATURES_PATH = r"C:\Users\edenm\Documents\GitHub\VoIP-Research\Data\features_extracted\processed_features_for_train.csv"
 
-data = pd.read_csv(PROCESSED_FEATURES_PATH).drop([r"Unnamed: 0", r"RTP_payload_type"], axis=1)
+# I dropped suspicious_diff feature, just because it's boolean not numeric, somtime it will be good practic to find a way to transfer it also to the network.
+data = pd.read_csv(PROCESSED_FEATURES_PATH).drop([r"Unnamed: 0", r"RTP_payload_type", "suspicious_diff"], axis=1)
 X = data.drop("isMalicious", axis=1)
 y = data.drop(data.columns.difference(["isMalicious"]), axis=1)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 scaler = StandardScaler()
-scaler.fit(X_train)    # Only the training data are used to fit the scaler transformation,
-X_train_scaled = scaler.transform(X_train)
-X_test_scaled = scaler.transform(X_test)  # then the scaler is used to transform the test input data.
-X_train_scaled = pd.DataFrame(X_train_scaled)
-X_test_scaled = pd.DataFrame(X_test_scaled)
+X_scaled = scaler.fit_transform(X)
 
-clf1 = AutoEncoder(hidden_neurons =[25, 2, 2, 25])
-clf1.fit(X_train_scaled)
+input_ = keras.layers.Input(shape=15,)
+dense1 = keras.layers.Dense(13, activation='relu')(input_)
+dense2 = keras.layers.Dense(11, activation='relu')(dense1)
+dense3 = keras.layers.Dense(8, activation='relu')(dense2)
+dense4 = keras.layers.Dense(4, activation='relu')(dense3)
+dense5 = keras.layers.Dense(8, activation='relu')(dense4)
+dense6 = keras.layers.Dense(11, activation='relu')(dense5)
+dense7 = keras.layers.Dense(13, activation='relu')(dense6)
+output = keras.layers.Dense(15, activation='linear')(dense7)
+model = keras.Model(input_, output)
 
-y_train_scores = clf1.decision_scores_
+# summarize layers
+print(model.summary())
 
-#y_train_scores
+model.compile("Adam", "mse")
 
-y_train_scores = clf1.decision_scores_  # raw outlier scores
+#model.fit(np.array(X_scaled[:-6]), np.array(X_scaled[:-6]), batch_size=1000, epochs=2)
+model.fit(np.array(X[:-6]).astype("float32"), np.array(X[:-6]).astype("float32"), batch_size=1000, epochs=2)
 
-# get the prediction on the test data
-y_test_pred = clf1.predict(X_test_scaled)  # outlier labels (0 or 1)
-# clf.decision_function: Predict raw anomaly score of X using the fitted detector.
-y_test_scores = clf1.decision_function(X_test_scaled)  # outlier scores
-
-y_test_pred = pd.Series(y_test_pred)
-y_test_scores = pd.Series(y_test_scores)
-
-plt.hist(y_test_scores, bins='auto')  # arguments are passed to np.histogram
-plt.title("Histogram for Model Anomaly Scores")
+AE_output = model.predict(np.array(X))
+#AE_output = model.predict(np.array(X_scaled))
+AE_deferences = sqrt((AE_output - X)**2)
+#AE_deferences = sqrt((AE_output - X_scaled)**2)
+AE_df = pd.DataFrame(AE_deferences, columns=data.drop(r"isMalicious", axis=1).columns)
+AE_df["grades"] = AE_df.apply(lambda row: np.linalg.norm(0 - row), axis=1)
+AE_df = AE_df.sort_values("grades")
+print(AE_df)
+plt.hist(AE_df)
 plt.show()
-
-print(clf1.predict(X_test_scaled))
-"""
-pca = PCA(2)
-x_pca = pca.fit_transform(X_train)
-x_pca = pd.DataFrame(x_pca)
-x_pca.columns=['PC1','PC2']
-x_pca.head()
-
-# Plot
-plt.scatter(X_train/2, X_train- X_train/2 ,alpha=0.8)
-plt.title('Scatter plot')
-plt.xlabel('PC1')
-plt.ylabel('PC2')
-plt.show()
-"""
-
-
-
