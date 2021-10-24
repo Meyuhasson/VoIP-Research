d5a768aabff354222085b1345114b4197f341f80
Meyuhasson <76136081+Meyuhasson@users.noreply.github.com>
2021-10-24 20:02:08 +0300

    update

--- Code/Python Files/AutoEncoder_classifier_with_randomize_data.py
+++ Code/Python Files/AutoEncoder_classifier_with_randomize_data.py
@@ -1,13 +1,12 @@
+# in this algorithm we'll aplly the same AutoEncoder Classifier, just over 10 times more data training,
+# which we generates with the same distribution as the original benign data
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
-
 import numpy as np
 from matplotlib import pyplot as plt
-from numpy import sqrt, array, random, argsort
+from numpy import sqrt
 from tensorflow import keras
-
-
+import scipy.stats as st
 
 PROCESSED_FEATURES_PATH = r"C:\Users\edenm\Documents\GitHub\VoIP-Research\Data\features_extracted\processed_features_for_train.csv"
 
@@ -17,14 +16,22 @@ X = data.drop("isMalicious", axis=1)
 
 #drop irrelevant for anomalous data features
 X = X.drop(["Lost_packets_count", "RTP_payload_length", "original_sr", r"RTP_payload_type", "suspicious_diff", "Lost_packets_precentage", "min_magnitude1", "min_magnitude2", "min_magnitude3"], axis=1)
-
 y = data.drop(data.columns.difference(["isMalicious"]), axis=1)
-#scaler = StandardScaler()
+
+df_temp = pd.DataFrame()
+for i in X.columns:
+
+    # generate 1000 random samples between the range of the specific column's benign values
+    samples = np.random.uniform(min(X[:-6][i].values), max(X[:-6][i].values), 1000)
+
+    #creating the new generated df
+    df_temp[i] = samples
+
+#append the new generated data df to the original
+X = X.append(df_temp)
+
 scaler = MinMaxScaler()
 X_scaled = scaler.fit_transform(X)
-
-
-
 
 input_ = keras.layers.Input(shape=8,)
 dense1 = keras.layers.Dense(7, activation='relu')(input_)
@@ -42,7 +49,7 @@ print(model.summary())
 
 model.compile("Adam", "mse")
 
-model.fit(np.array(X_scaled[:-6]), np.array(X_scaled[:-6]), batch_size=6, epochs=150)
+model.fit(np.array(X_scaled[:-6]), np.array(X_scaled[:-6]), batch_size=12, epochs=1500)
 #model.fit(np.array(X[:-6]).astype("float32"), np.array(X[:-6]).astype("float32"), batch_size=12, epochs=10000)
 
 #AE_output = model.predict(np.array(X))
