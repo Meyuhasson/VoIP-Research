38016ede0cf9d95ca905060a5ec868160eea49cb
Meyuhasson <76136081+Meyuhasson@users.noreply.github.com>
2022-01-10 06:32:15 +0200

    dbscan update

--- Code/Python Files/ploty.py
+++ Code/Python Files/ploty.py
@@ -8,7 +8,7 @@ from sklearn.decomposition import PCA
 
 
 
-PROCESSED_FEATURES_PATH = r"C:\Users\edenm\Documents\GitHub\VoIP-Research\Data\features_extracted\processed_features_for_train.csv"
+PROCESSED_FEATURES_PATH = r"C:\Users\edenm\Documents\GitHub\VoIP-Research\Data\features_extracted\‏‏processed_features_for_train_no_imputing.csv"
 
 # I dropped suspicious_diff feature, just because it's boolean not numeric, somtime it will be good practic to find a way to transfer it also to the network.
 data = pd.read_csv(PROCESSED_FEATURES_PATH).drop([r"Unnamed: 0"], axis=1)
@@ -19,16 +19,14 @@ X = X.drop(["Lost_packets_count", "RTP_payload_length", "original_sr", "suspicio
 
 y = data.drop(data.columns.difference(["isMalicious"]), axis=1)
 
-scaler = MinMaxScaler()
-X_scaled = scaler.fit_transform(X)
+#scaler = MinMaxScaler()
+#X_scaled = scaler.fit_transform(X)
 
 pca_model = PCA(n_components=2)
-data_transformed = pca_model.fit_transform(X[:-5])
-DS_utils.plot_points_scatter(X, data, "data before clustering")
+data_transformed = pca_model.fit_transform(X)
 
-dbs_classifier = DBSCAN(min_samples=8, eps=0.75192)
-dbs_outputs = dbs_classifier.fit_predict(X_scaled[:-5])
-print(dbs_outputs)
+y = y.replace(False,0)
+y = y.replace(True,1)
 colors = dict(zip([0,1]+[-1], [0.1, 0.5, 0.9]))
-plt.scatter(data_transformed[:,0], data_transformed[:,1], c = list(map(lambda x: colors[x], dbs_outputs)))
+plt.scatter(data_transformed[:,0], data_transformed[:,1], c = list(map(lambda x: colors[x], y["isMalicious"].tolist())))
 plt.show()
